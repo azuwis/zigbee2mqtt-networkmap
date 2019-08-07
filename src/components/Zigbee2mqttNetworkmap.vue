308944d59ea44fc9ad48da854d0e0b741257b4c1
Zhong Jianxin <azuwis@gmail.com>
2019-08-07 10:05:36 +0800

    Remove link-start

--- src/components/Zigbee2mqttNetworkmap.vue
+++ src/components/Zigbee2mqttNetworkmap.vue
@@ -6,9 +6,6 @@
                 <marker id="m-end" markerWidth="10" markerHeight="10" refX="9" refY="3" orient="auto" markerUnits="strokeWidth" >
                     <path d="M0,0 L0,6 L9,3 z"></path>
                 </marker>
-                <marker id="m-start" markerWidth="6" markerHeight="6" refX="-4" refY="3" orient="auto" markerUnits="strokeWidth" >
-                    <rect width="2" height="6"></rect>
-                </marker>
             </defs>
         </svg>
     </div>
@@ -63,8 +60,7 @@
      },
      methods: {
          link_cb(link) {
-             link._svgAttrs = { 'marker-end': 'url(#m-end)',
-                                'marker-start': 'url(#m-start)'}
+             link._svgAttrs = { 'marker-end': 'url(#m-end)' }
              return link
          }
      }
@@ -73,7 +69,7 @@
 
 <style>
  canvas{position:absolute;top:0;left:0}.net{height:100%;margin:0}.node{stroke:rgba(18,120,98,.7);stroke-width:3px;-webkit-transition:fill .5s ease;transition:fill .5s ease;fill:#dcfaf3}.node.selected{stroke:#caa455}.node.pinned{stroke:rgba(190,56,93,.6)}.link{stroke:rgba(18,120,98,.3)}.link,.node{stroke-linecap:round}.link:hover,.node:hover{stroke:#be385d;stroke-width:5px}.link.selected{stroke:rgba(202,164,85,.6)}.curve{fill:none}.link-label,.node-label{fill:#127862}.link-label{-webkit-transform:translateY(-.5em);transform:translateY(-.5em);text-anchor:middle}
- #m-end path, #m-start{
+ #m-end path {
      fill: rgba(18, 120, 98, 0.7);
  }
 </style>
