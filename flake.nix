d5f1002118ba5881c6bdc27cb0f67642575c414f
Zhong Jianxin <azuwis@gmail.com>
2023-12-06 20:27:16 +0800

    flake: Remove flake-utils, use github:nix-systems

--- flake.nix
+++ flake.nix
@@ -1,30 +1,38 @@
 {
-  inputs.nixpkgs.url = "nixpkgs";
-  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.systems.url = "github:nix-systems/default";
   inputs.devshell.url = "github:numtide/devshell";
   inputs.devshell.inputs.nixpkgs.follows = "nixpkgs";
-  inputs.devshell.inputs.flake-utils.follows = "flake-utils";
+  inputs.devshell.inputs.systems.follows = "systems";
 
-  outputs = { self, nixpkgs, flake-utils, devshell }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = import nixpkgs {
-        inherit system;
-        overlays = [ devshell.overlay ];
-      };
-      nodeModules = pkgs.mkYarnModules {
-        pname = "zigbee2mqtt-networkmap-node-modules";
-        version = "1.0";
-        packageJSON = ./package.json;
-        yarnLock = ./yarn.lock;
-      };
-    in {
-      devShell = pkgs.devshell.mkShell {
-        devshell.startup.yarn.text = ''
-          ${pkgs.rsync}/bin/rsync -rlt --chmod=Du+w --delete ${nodeModules}/node_modules ./
-        '';
-        packages = with pkgs; [
-          yarn
-        ];
-      };
-    });
+  outputs = inputs@{ ... }:
+    let
+      eachSystem = inputs.nixpkgs.lib.genAttrs (import inputs.systems);
+    in
+    {
+      devShells = eachSystem (system:
+        let
+          pkgs = inputs.nixpkgs.legacyPackages.${system};
+          devshell = import inputs.devshell {
+            inherit inputs system;
+            nixpkgs = pkgs;
+          };
+          nodeModules = pkgs.mkYarnModules {
+            pname = "zigbee2mqtt-networkmap-node-modules";
+            version = "1.0";
+            packageJSON = ./package.json;
+            yarnLock = ./yarn.lock;
+          };
+        in
+        {
+          default = devshell.mkShell {
+            devshell.startup.yarn.text = ''
+              ${pkgs.rsync}/bin/rsync -rlt --chmod=Du+w --delete ${nodeModules}/node_modules ./
+            '';
+            packages = with pkgs; [
+              yarn
+            ];
+          };
+        });
+    };
 }
