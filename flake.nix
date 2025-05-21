760d177d5b47a37b05e791b88cf1b515344b1c8c
Zhong Jianxin <azuwis@gmail.com>
2025-05-21 21:12:02 +0800

    flake: Bump nixpkgs to nixos-25.05

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
   inputs.devshell.url = "github:numtide/devshell";
   inputs.devshell.inputs.nixpkgs.follows = "nixpkgs";
 
