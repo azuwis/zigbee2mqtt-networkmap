6354babeee945218dda2160731470e003615a15a
Zhong Jianxin <azuwis@gmail.com>
2026-05-20 19:20:49 +0800

    chore: replace @vue/eslint-config-standard with @eslint/js + globals

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,21 @@
+import js from '@eslint/js'
 import pluginVue from 'eslint-plugin-vue'
-import standard from '@vue/eslint-config-standard'
+import globals from 'globals'
 
 export default [
+  js.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        ...globals.browser,
+        ...globals.es2021
+      }
+    }
+  },
   ...pluginVue.configs['flat/essential'].map(config => ({
     ...config,
     files: ['**/*.vue']
-  })),
-  ...standard
+  }))
 ]
